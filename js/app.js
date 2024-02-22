ae03fea96513dd6a2b076448eaa080389b51d4ce
Thiago1469 <158086949+Thiago1469@users.noreply.github.com>
2024-02-22 15:02:43 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 var btn = document.getElementById("menu-button");
 var mainMenu = document.getElementById("mobile-menu");
 
-btn.addEventListener("click", function(){
+mainMenu.style.display = "none";
+
+    function toggleMenu(){
     if(mainMenu.style.display === "flex"){
-        mainMenu.style.display = "none"
+        mainMenu.style.display = "none";
+
     } else{
-        mainMenu.style.display = "flex"
+        mainMenu.style.display = "flex";
+        mainMenu.classList.add("adicional");
+    }
+
+}
+
+btn.addEventListener("click", toggleMenu);
+
+window.addEventListener("resize", function(){
+    if(window.innerWidth <= 721){
+        mainMenu.style.display = "none";
     }
 })
